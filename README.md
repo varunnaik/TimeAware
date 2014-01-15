d00b226716f7d8cc910738ad69784f61ebb9a10b
Varun Naik <varun.naik@gmail.com>
2014-01-15 18:55:57 +0530

    Start extension in earnest, add metadata, and rough algorithm layout.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ TimeAware
 =========
 
 Browser extension to keep yourself aware of how you spend time on the Internet
+
+Features
+========
+
+Displays a timer in the lower right of your screen telling you how long you have been on a site
+Only appears after you are on a site for longer than an hour
+Will appear only on sites you define in a list
+Can provide detailed stats on browsing behaviour
+
+Planned features:
+Cloud upload of sites & times (encrypted)
+Firefox version
+
+
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,3 +3,20 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
+
+/* Development notes (delete before release)
+ Data to store:
+ 1. URL   - config: treat http and https versions of the site as the same site
+ overall time
+ today's date
+ time today
+ 3. blocklist
+ 4. settings
+
+ Workflow:
+ hook: On Tab create/window create/navigation finish:
+    Run extension code
+    Add site to list of sites
+    If settimeout handle empty create one now
+
+ */
